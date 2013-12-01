5b9e25830f7de2118c44f221b159eea8703a40aa
Joah Gerstenberg <joahd@gbergfamily.com>
2013-12-01 07:54:56 -0600

    Created blank Rails app to handle Under Construction page.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UnderConstruction::Application.config.secret_token = 'd3b9a43e837a52112d9885229d009b108a49ca2a6dafb49d5d4cdbd8fbc32a501f382c99210a6576af72e3df1ff30b061327f922d279baffd6d7b439a3f9ae4f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+UnderConstruction::Application.config.session_store :cookie_store, key: '_under_construction_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# UnderConstruction::Application.config.session_store :active_record_store
